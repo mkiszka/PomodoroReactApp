433120b581644344350cbdb95a2d99eb121bd2ed
mkiszka <github@kiszka.name>
2022-06-11 10:35:59 +0200

    Migrated Css to SCSS.
    Added node-sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./styles/main.css"
+import "./styles/main.scss"
 
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
