d04e5786a1c6de59f6c13e24235743b10e7197cd
mkiszka <github@kiszka.name>
2022-06-12 07:41:00 +0200

    Restyled TimeboxCreator

--- src/components/TimeboxCreator.js
+++ src/components/TimeboxCreator.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { IconContext } from "react-icons/lib";
 
 class TimeboxCreator extends React.Component {
 
@@ -24,13 +26,22 @@ class TimeboxCreator extends React.Component {
         } = this.props;
 
 
-        return (<form ref={this.form} onSubmit={this.handleSubmit} className={`TimeboxCreator ${isEditable ? "" : "inactive"}`}>
-            <label>Co robisz ?<input disabled={!isEditable} value={title} type="text"
-                onChange={onTitleChange} /></label><br />
-            <label>Ile minut ?<input disabled={!isEditable} value={totalTimeInMinutes} type="number"
-                onChange={onTotalTimeInMinutesChange} /></label><br />
-            <button disabled={!isEditable}>Dodaj</button>
-        </form>
+        return (
+            <IconContext.Provider value={{ className: "" }}>
+                <form ref={this.form} onSubmit={this.handleSubmit} className={`TimeboxCreator ${isEditable ? "" : "inactive"}`}>
+                    <div>
+                        <label>Co robisz ?<input disabled={!isEditable} value={title} type="text"
+                            onChange={onTitleChange} /></label>
+                        <label>Ile minut ?<input disabled={!isEditable} value={totalTimeInMinutes} type="number"
+                            onChange={onTotalTimeInMinutesChange} /></label>
+                    </div>
+                    <div>
+                        <button type="submit">
+                            <IoAddCircleOutline disabled={!isEditable} className={`${isEditable ? "button-active" : "button-inactive"}`} />
+                        </button>
+                    </div>
+                </form>
+            </IconContext.Provider>
         );
     }
 }
