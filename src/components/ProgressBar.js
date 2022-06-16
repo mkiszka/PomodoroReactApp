de1b1ccac396a55fb411892504c7f4e88f1f65ca
mkiszka <github@kiszka.name>
2022-06-16 22:39:28 +0200

    Added range validation in ProgressBar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -12,13 +12,25 @@ function ProgressBar({ percent, trackRemaining, className }) {
         </div>
     );
 }
+
+//min, max, 
+function validateNumberRange(min, max) {
+    return (prop, propName, componentName) => {
+        const val = prop[propName];
+        if (!(val >= min && val <= max)) {
+            return new Error(`${propName} in ${componentName} is not within [${min},${max}]`);
+        }
+    }
+
+}
+
 ProgressBar.defaultProps = {
     percent: 0,
-    trackRemaining: false,    
+    trackRemaining: false,
 }
 
 ProgressBar.propTypes = {
-    percent: PropTypes.number,
+    percent: validateNumberRange(0, 100),
     trackRemaining: PropTypes.bool,
     className: PropTypes.string
 
