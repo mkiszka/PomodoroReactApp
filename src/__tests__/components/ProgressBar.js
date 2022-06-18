329de3e1330690c4393cbb168a953e3482510333
mkiszka <github@kiszka.name>
2022-06-18 09:46:02 +0200

    Added, the ProgressBar is checked whether it matches the snapshoot.

--- src/__tests__/components/ProgressBar.js
+++ src/__tests__/components/ProgressBar.js
@@ -1,29 +1,36 @@
 import TestRenderer from 'react-test-renderer';
 import ProgressBar from '../../components/ProgressBar';
 
-describe('ProgressBar should', () => {
+describe('ProgressBar ', () => {
     let progressBar = null;
     let progressBarJSON = null;
-    beforeAll(() => {
-        progressBar = TestRenderer.create(<ProgressBar />);
-        progressBarJSON = progressBar.toJSON();
-    })
-    it('not contain undefined className', () => {
-        expect(progressBarJSON.props.className).not.toMatch(/undefined/);
-    });
-    /*
-    {
-          type: 'div',
-          props: {
-            className: 'ProgressBar ProgressBar_trackRemaining_false undefined ',
-            style: { '--width': '0%', '--background-color': 'red' }
-          },
-          children: null
-        }
-        */
 
-    // it(' .... ', () => {
-    //     console.log(progressBar.toJSON());
-    //     expect(progressBar.toJSON()).toBe();
-    // })
+    describe('when none of parameters was set, should ', () => {
+        beforeAll(() => {
+            progressBar = TestRenderer.create(<ProgressBar />);
+            progressBarJSON = progressBar.toJSON();
+        })
+        it('not contain undefined className', () => {
+            expect(progressBarJSON.props.className).not.toMatch(/undefined/);
+        });
+        it('looks always the same',()=> {
+            expect(progressBarJSON).toMatchSnapshot();
+        });
+        /*
+        {
+              type: 'div',
+              props: {
+                className: 'ProgressBar ProgressBar_trackRemaining_false undefined ',
+                style: { '--width': '0%', '--background-color': 'red' }
+              },
+              children: null
+            }
+            */
+/*
+        it(' .... ', () => {
+            console.log(progressBar.toJSON());
+            expect(progressBar.toJSON()).toBe();
+        })
+        */
+    });
 });
