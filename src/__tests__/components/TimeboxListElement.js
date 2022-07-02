3dd04fb4fdc4515b091f21bef3904391248ae9da
mkiszka <github@kiszka.name>
2022-07-02 11:17:22 +0200

    Refactored according to eslint tips - TimeboxListElement tests.

--- src/__tests__/components/TimeboxListElement.js
+++ src/__tests__/components/TimeboxListElement.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render, cleanup, fireEvent } from '@testing-library/react';
+import { screen, render, cleanup } from '@testing-library/react';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import TimeboxListElement from "../../components/TimeboxListElement";
@@ -18,90 +18,86 @@ describe('TimeboxListElement', () => {
         beforeEach(() => {
             timebox = { uid: uuid(), title: "Wywołanie eventów", totalTimeInMinutes: 3, isEditable: false };
         });
-        it(' cannot be edited', () => {
+        it('cannot be edited', () => {
 
-            const { getByRole } = render(<DndProvider backend={HTML5Backend}><TimeboxListElement timebox={timebox} uuid={timebox.uid} findElement={findElement} /></DndProvider>);
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement timebox={timebox} uuid={timebox.uid} findElement={findElement} /></DndProvider>);
 
-            expect(getByRole('textbox')).toBeDisabled();
-            expect(getByRole('spinbutton')).toBeDisabled();
+            expect(screen.getByRole('textbox')).toBeDisabled();
+            expect(screen.getByRole('spinbutton')).toBeDisabled();
 
         });
-        it(' has edit button', () => {
+        it('has edit button', () => {
 
-            const { queryByTitle, getByTitle } =
-                render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                     timebox={timebox}
                     uuid={timebox.uid}
                     findElement={findElement}
                 /></DndProvider>);
 
-            expect(getByTitle("edytuj")).toBeInTheDocument();
-            expect(queryByTitle("zapisz")).toBeNull();
+            expect(screen.getByTitle("edytuj")).toBeInTheDocument();
+            expect(screen.queryByTitle("zapisz")).toBeNull();
         });
-        it(' has start button', () => {
+        it('has start button', () => {
 
-            const { queryByTitle, getByTitle } =
-                render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+           
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                     timebox={timebox}
                     uuid={timebox.uid}
                     findElement={findElement}
                 /></DndProvider>);
 
-            expect(getByTitle("start")).toBeInTheDocument();
+            expect(screen.getByTitle("start")).toBeInTheDocument();
         });
     });
     describe('isEditable true', () => {
         beforeEach(() => {
             timebox = { uid: uuid(), title: "Wywołanie eventów", totalTimeInMinutes: 3, isEditable: true };
         });
-        it(' should set proper value of textarea and input value (title=Wywłanie eventów, totalTimeInMinutes=3)', () => {
+        it('should set proper value of textarea and input value (title=Wywłanie eventów, totalTimeInMinutes=3)', () => {
 
 
-            const { getByRole } = render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                 timebox={timebox}
                 uuid={timebox.uid}
                 findElement={findElement}
             /></DndProvider>);
 
-            expect(getByRole('textbox').value).toMatch("Wywołanie eventów");
-            expect(getByRole('spinbutton').value).toEqual("3");
+            expect(screen.getByRole('textbox').value).toMatch("Wywołanie eventów");
+            expect(screen.getByRole('spinbutton').value).toEqual("3");
         })
-        it(' can be edited', () => {
-
-            const { getByRole } =
-                render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+        it('can be edited', () => {
+            
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                     timebox={timebox}
                     uuid={timebox.uid}
                     findElement={findElement}
                 /></DndProvider>);
 
-            expect(getByRole('textbox')).not.toBeDisabled();
-            expect(getByRole('spinbutton')).not.toBeDisabled();
+            expect(screen.getByRole('textbox')).not.toBeDisabled();
+            expect(screen.getByRole('spinbutton')).not.toBeDisabled();
 
         });
 
-        it(' has delete button', () => {
+        it('has delete button', () => {
 
-            const { getByTitle } =
-                render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                     timebox={timebox}
                     uuid={timebox.uid}
                     findElement={findElement}
                 /></DndProvider>);
-            expect(getByTitle("usuń")).toBeInTheDocument();
+            expect(screen.getByTitle("usuń")).toBeInTheDocument();
 
         });
 
-        it(' has save button', () => {
-
-            const { queryByTitle, getByTitle } =
-                render(<DndProvider backend={HTML5Backend}><TimeboxListElement
+        it('has save button', () => {
+            
+            render(<DndProvider backend={HTML5Backend}><TimeboxListElement
                     timebox={timebox}
                     uuid={timebox.uid}
                     findElement={findElement}
                 /></DndProvider>);
-            expect(getByTitle("zapisz")).toBeInTheDocument();
-            expect(queryByTitle("edytuj")).toBeNull();
+            expect(screen.getByTitle("zapisz")).toBeInTheDocument();
+            expect(screen.queryByTitle("edytuj")).toBeNull();
         });
     })
 });
