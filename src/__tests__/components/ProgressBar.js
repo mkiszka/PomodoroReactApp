e8e8bd49089a1a3b0a5c9669adfaefc57c0c9034
mkiszka <github@kiszka.name>
2022-07-02 11:10:58 +0200

    Refactored according to eslint tips - ProgressBar tests.

--- src/__tests__/components/ProgressBar.js
+++ src/__tests__/components/ProgressBar.js
@@ -4,21 +4,21 @@ import ProgressBar from "../../components/ProgressBar";
 
 
 
-describe('ProgressBar ', () => {
+describe('ProgressBar', () => {
     let progressBar = null;
     let progressBarJSON = null;
    
     describe('when trackRemaining', () => {
-        it(' is true className should contain  ProgressBar_trackRemaining_true', () => {
+        it('is true className should contain  ProgressBar_trackRemaining_true', () => {
             progressBarJSON = TestRenderer.create(<ProgressBar percent={80} trackRemaining={true} />).toJSON();
             expect(progressBarJSON.props.className).toMatch('ProgressBar_trackRemaining_true');
         })
-        it(' is false className should contain  ProgressBar_trackRemaining_true', () => {
+        it('is false className should contain  ProgressBar_trackRemaining_true', () => {
             progressBarJSON = TestRenderer.create(<ProgressBar percent={80} trackRemaining={false} />).toJSON();
             expect(progressBarJSON.props.className).toMatch('ProgressBar_trackRemaining_false');
         })
     });
-    describe('when none of parameters was set, should ', () => {
+    describe('when none of parameters was set, should', () => {
         beforeAll(() => {
             progressBar = TestRenderer.create(<ProgressBar />);
             progressBarJSON = progressBar.toJSON();
