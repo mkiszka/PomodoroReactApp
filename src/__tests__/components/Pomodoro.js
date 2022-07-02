e6475b231a1c0c871d8af4d2aac296f9db127345
mkiszka <github@kiszka.name>
2022-07-02 11:06:46 +0200

    Refactored according to eslint tips - Pomodoro test.

--- src/__tests__/components/Pomodoro.js
+++ src/__tests__/components/Pomodoro.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { render, fireEvent, within } from "@testing-library/react";
+import { render, fireEvent, within, screen } from "@testing-library/react";
 import Pomodoro from "../../components/Pomodoro";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 describe('Pomodoro', () => {
     it('should change element position on enter', () => {
-        const { getAllByRole } = render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
-        const list = getAllByRole('listitem');
+        render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
+        const list = screen.getAllByRole('listitem');
         const firstElement = list[0];
         const lastElement = list[2];
 
@@ -17,7 +17,7 @@ describe('Pomodoro', () => {
         fireEvent.dragStart(lastElement);
         fireEvent.dragEnter(firstElement); 
 
-        const newFirstElement = getAllByRole('listitem')[0];
+        const newFirstElement = screen.getAllByRole('listitem')[0];
         firstElementTitle = within(newFirstElement).getByRole('textbox');
     
         expect(firstElementTitle.value).toEqual("KP-3104 Deploy webserwisu zamówień dla 1.15");
@@ -26,8 +26,8 @@ describe('Pomodoro', () => {
 
     })
     it.skip('should change element position on drag over', () => {
-        const { getAllByRole } = render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
-        const list = getAllByRole('listitem');
+        render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
+        const list = screen.getAllByRole('listitem');
         const firstElement = list[0];
         const lastElement = list[2];
 
@@ -37,7 +37,7 @@ describe('Pomodoro', () => {
         fireEvent.dragStart(lastElement);        
         fireEvent.dragOver(firstElement);
     
-        const newFirstElement = getAllByRole('listitem')[0];
+        const newFirstElement = screen.getAllByRole('listitem')[0];
         firstElementTitle = within(newFirstElement).getByRole('textbox');
     
         expect(firstElementTitle.value).toEqual("KP-3104 Deploy webserwisu zamówień dla 1.15");
@@ -46,8 +46,8 @@ describe('Pomodoro', () => {
 
     })
     it('should change element position on drop', () => {
-        const { getAllByRole } = render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
-        const list = getAllByRole('listitem');
+        render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
+        const list = screen.getAllByRole('listitem');
         const firstElement = list[0];
         const lastElement = list[2];
 
@@ -57,25 +57,25 @@ describe('Pomodoro', () => {
         fireEvent.dragStart(lastElement);        
         fireEvent.drop(firstElement);
 
-        const newFirstElement = getAllByRole('listitem')[0];
+        const newFirstElement = screen.getAllByRole('listitem')[0];
         firstElementTitle = within(newFirstElement).getByRole('textbox');
     
         expect(firstElementTitle.value).toEqual("KP-3104 Deploy webserwisu zamówień dla 1.15");
 
     })
-    it('start button should change Timebox content ', () => {
+    it('start button should change Timebox content', () => {
        
-        const { getByTestId, getAllByRole,debug } = render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
-        // debug();
-        let timebox = getByTestId('Timebox');
+        render(<DndProvider backend={HTML5Backend}><Pomodoro/></DndProvider>);
+        
+        let timebox = screen.getByTestId('Timebox');
         const taskTitle = within(timebox).getByRole('heading', { level: 1 });
         expect(taskTitle.textContent).toEqual("Wywołanie eventów");
 
-        const taskList = getAllByRole('listitem');
+        const taskList = screen.getAllByRole('listitem');
         const startButton = within(taskList[1]).getByTitle('start');
         fireEvent.click(startButton);
 
-        timebox =  getByTestId('Timebox');    
+        timebox =  screen.getByTestId('Timebox');    
         expect(taskTitle.textContent).toEqual("KP-3034 Migracja z ver 1.14 do 1.15 usuwa powiązanie pacjent pracownik.");
     })
 });
