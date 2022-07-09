1cceee02cbd3bc72e494193e108a70ab37822bb5
mkiszka <github@kiszka.name>
2022-07-09 21:18:47 +0200

    Removed using 'new String' statement

--- src/components/RealTimeClock.js
+++ src/components/RealTimeClock.js
@@ -12,8 +12,8 @@ class RealTimeClock extends React.Component {
         const date = new Date();
         return {
             hours: date.getHours(), 
-            minutes: parseInt(new String(date.getMinutes()).padStart(2,0)), 
-            seconds: parseInt(new String(date.getSeconds()).padStart(2,0))};
+            minutes: parseInt(date.getMinutes().toString().padStart(2,0)), 
+            seconds: parseInt(date.getSeconds().toString().padStart(2,0))};
     }
 
     componentDidMount() {    
