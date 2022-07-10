eb0c6de2d8a26aabd2e0743f45e03c9e03d9e115
mkiszka <github@kiszka.name>
2022-07-10 21:19:27 +0200

    Added fake API

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
-import { instanceOf } from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
-import { withCookies, Cookies } from 'react-cookie';
+import CookiesApi from 'universal-cookie';
 import { DraggableItemTypes } from "./DraggableItemTypes";
 import { useDrop } from "react-dnd";
 
@@ -15,51 +14,55 @@ import ErrorMessage from "./ErrorMessage";
 import withAutoIndicator from "./AutoIndicator";
 import ProgressBar from './ProgressBar';
 
-// function initialTimeboxes(cookies) {
-//     return cookies.get('timeboxes') ||
-//         [
-//             { uid: uuidv4(), title: "Wywołanie eventów", totalTimeInMinutes: 3, isEditable: false },
-//             { uid: uuidv4(), title: "KP-3034 Migracja z ver 1.14 do 1.15 usuwa powiązanie pacjent pracownik.", totalTimeInMinutes: 20, isEditable: false },
-//             { uid: uuidv4(), title: "KP-3104 Deploy webserwisu zamówień dla 1.15", totalTimeInMinutes: 20, isEditable: false },
-//         ];
-// }
 function wait(ms) {
     return new Promise((resolve) => { setTimeout(() => resolve(), ms) });
 }
-//dodanie funkcji symulującej pobieranie danych z serwera
-//TimeboxesApi (symulowanie oóznienia)
-// pobieranie z api
-// dodawanie do api
-// podmianka przy aktualizacji
-//zadania
 
+const COOKIE_TIMEBOXES = 'timeboxes';
+const cookies = new CookiesApi();
 const TimeboxAPI = {
-    getAllTimeboxes: async function () {
-        await wait(3000);
-        return [
+    timeboxes: cookies.get(COOKIE_TIMEBOXES, { path: '/' }) ||
+        [
             { uid: uuidv4(), title: "Wywołanie eventów", totalTimeInMinutes: 3, isEditable: false },
             { uid: uuidv4(), title: "KP-3034 Migracja z ver 1.14 do 1.15 usuwa powiązanie pacjent pracownik.", totalTimeInMinutes: 20, isEditable: false },
             { uid: uuidv4(), title: "KP-3104 Deploy webserwisu zamówień dla 1.15", totalTimeInMinutes: 20, isEditable: false },
-        ];
+        ],
+    getAllTimeboxes: async function () {
+        await wait(300);        
+        return [...this.timeboxes];
     },
+
     addTimebox: async function (timeboxToAdd) {
-        await wait(3000);
+        await wait(1000);
+        this.timeboxes.splice(0, 0, timeboxToAdd)
+        this.updateTimeboxesInsideCookie();
+
     },
-    replaceTimebox: async function (timeboxToReplace) {
+    replaceTimebox: async function (timeboxToReplace) {        
         await wait(3000);
+        this.timeboxes = this.timeboxes.map((value) => value.uid === timeboxToReplace.uid ? timeboxToReplace : value)
+        this.updateTimeboxesInsideCookie();        
     },
-    removeTimebox: async function (timeboxToRemove) {
-        await wait(3000);
-        //timeboxes.splice
+    removeTimebox: async function (uid) {
+        console.log("Api.removetimebox 1");
+        await wait(1000);
+        this.timeboxes = this.timeboxes.filter((value) => value.uid === uid ? false : true);
+        console.log("Api.removetimebox 2");
+        this.updateTimeboxesInsideCookie();
+        console.log(cookies.get(COOKIE_TIMEBOXES, { path: '/' }));
+    },
+    updateTimeboxesInsideCookie: function () {
+        cookies.set(COOKIE_TIMEBOXES, this.timeboxes, { path: '/' });
     }
 };
 
-function Pomodoro({ cookies }) {
-    //initialTimeboxes(cookies)
+function Pomodoro() {
+
     const [timeboxes, setTimeboxes] = useState([]);
     useEffect(() => {
+
         TimeboxAPI.getAllTimeboxes()
-            .then((timeboxes) => setTimeboxes(timeboxes))
+            .then((timeboxes) => { setTimeboxes(timeboxes) })
             .catch((error) => setLoadingError(error))
             .finally(() => setIsLoding(false));
     }, []);
@@ -72,13 +75,16 @@ function Pomodoro({ cookies }) {
     const [loadingError, setLoadingError] = useState(null);
 
     function handleDeleteTimeboxListElement(uid) {
-        setTimeboxes(
-            (prevTimeboxes) => {
-                let timeboxes = prevTimeboxes.filter((value, index) => value.uid === uid ? false : true);
-                cookies.set('timeboxes', timeboxes, { path: '/' });
-                return timeboxes;
-            }
-        )
+        TimeboxAPI.removeTimebox(uid).then(() => {
+            console.log("removeTimebox.then");
+            setTimeboxes(
+                (prevTimeboxes) => {
+                    let timeboxes = prevTimeboxes.filter((value, index) => value.uid === uid ? false : true);
+                    return timeboxes;
+                }
+            )
+        }
+        );
     }
 
     function handleTitleCreatorChange(event) {
@@ -88,25 +94,38 @@ function Pomodoro({ cookies }) {
     function handleTotalTimeInMinutesCreatorChange(event) {
         setTotalTimeInMinutes(event.target.value);
     }
+
     function handleCreatorAdd(timeboxToAdd) {
-        setTimeboxes(
-            (prevTimeboxes) => {
-                let timeboxes = prevTimeboxes;
-                timeboxes = [...prevTimeboxes, timeboxToAdd];
-                cookies.set('timeboxes', timeboxes, { path: '/' });
-                return timeboxes;
-            }
-        )
+        TimeboxAPI.addTimebox({ ...timeboxToAdd }).then(() => {
+            setTimeboxes(
+                (prevTimeboxes) => {
+                    return [timeboxToAdd, ...prevTimeboxes];
+                }
+            )
+        });
     }
 
-    function handleEditTimeboxListElement(uid) {
+    function handleEditTimeboxListElement(uid) {          
+            setTimeboxes(
+                (prevTimeboxes) => {
+                    return prevTimeboxes.map((value) => {
+                        return value.uid === uid ? { ...value, isEditable: true } : value
+                    })
+                }
+            )
+    }
 
-        setTimeboxes(
-            (prevTimeboxes) => {
-                return prevTimeboxes.map((value) => {
-                    return value.uid === uid ? { ...value, isEditable: !value.isEditable } : value
-                })
-            }
+    function handleSaveTimeboxListElement(uid) {
+        const { element } = findElement(uid);
+
+        TimeboxAPI.replaceTimebox({ ...element, isEditable: false }).then(
+            setTimeboxes(
+                (prevTimeboxes) => {
+                    return prevTimeboxes.map((value) => {
+                        return value.uid === uid ? { ...value, isEditable: false } : value
+                    })
+                }
+            )
         )
     }
 
@@ -165,10 +184,11 @@ function Pomodoro({ cookies }) {
         },
         [findElement, timeboxes],
     )
-    const AutoIndicator = withAutoIndicator(ProgressBar); //TODO - fix the refresh bug
+    
+    const AutoIndicator = withAutoIndicator(ProgressBar); //TODO - fix the refresh bug    
 
     const [, drop] = useDrop(() => ({ accept: DraggableItemTypes.TimeboxListElement }))
-    console.log("🚀 ~ file: Pomodoro.js ~ line 159 ~ Pomodoro ~ isLoading", isLoading)
+    console.log("🚀 ~ file: Pomodoro.js ~ line 182 ~ Pomodoro ~ isLoading", isLoading)
     return (
         <>
             {/* <AutoIndicator /> */}
@@ -179,7 +199,7 @@ function Pomodoro({ cookies }) {
                 onAdd={handleCreatorAdd}
                 isEditable={isEditable} />
             {loadingError ? <ErrorMessage error={loadingError} /> : ""}
-            {isLoading ? <AutoIndicator refresh="10"/> : ""}
+            {isLoading ? <AutoIndicator refresh="10" /> : ""}
             {timeboxes.length > 0 ? <Timebox
                 timebox={timeboxes.length > 0 ? timeboxes[timeboxes_currentIndex] : {}}
                 isEditable={true}
@@ -195,6 +215,7 @@ function Pomodoro({ cookies }) {
                             onTitleChange={handleTitleElementChange}
                             onTimeChange={handleTimeElementChange}
                             onEdit={() => { handleEditTimeboxListElement(elem.uid) }}
+                            onSave={() => { handleSaveTimeboxListElement(elem.uid)}}
                             onDelete={() => { handleDeleteTimeboxListElement(elem.uid) }}
                             onStart={() => { handleStartTimeboxListElement(index) }}
                             moveElement={moveElement}
@@ -208,7 +229,5 @@ function Pomodoro({ cookies }) {
 
     )
 }
-Pomodoro.propTypes = {
-    cookies: instanceOf(Cookies).isRequired
-}
-export default withCookies(Pomodoro);
+
+export default Pomodoro;
