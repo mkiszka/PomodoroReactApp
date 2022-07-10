f52690bd0361f9b534c782f4e9b0f0ef2273b839
mkiszka <github@kiszka.name>
2022-07-10 21:37:52 +0200

    Moved TimeboxAPI to TimeboxFakeAPI

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import CookiesApi from 'universal-cookie';
+
 import { DraggableItemTypes } from "./DraggableItemTypes";
 import { useDrop } from "react-dnd";
 
@@ -13,48 +12,7 @@ import TimeboxCreator from "./TimeboxCreator";
 import ErrorMessage from "./ErrorMessage";
 import withAutoIndicator from "./AutoIndicator";
 import ProgressBar from './ProgressBar';
-
-function wait(ms) {
-    return new Promise((resolve) => { setTimeout(() => resolve(), ms) });
-}
-
-const COOKIE_TIMEBOXES = 'timeboxes';
-const cookies = new CookiesApi();
-const TimeboxAPI = {
-    timeboxes: cookies.get(COOKIE_TIMEBOXES, { path: '/' }) ||
-        [
-            { uid: uuidv4(), title: "Wywołanie eventów", totalTimeInMinutes: 3, isEditable: false },
-            { uid: uuidv4(), title: "KP-3034 Migracja z ver 1.14 do 1.15 usuwa powiązanie pacjent pracownik.", totalTimeInMinutes: 20, isEditable: false },
-            { uid: uuidv4(), title: "KP-3104 Deploy webserwisu zamówień dla 1.15", totalTimeInMinutes: 20, isEditable: false },
-        ],
-    getAllTimeboxes: async function () {
-        await wait(300);        
-        return [...this.timeboxes];
-    },
-
-    addTimebox: async function (timeboxToAdd) {
-        await wait(1000);
-        this.timeboxes.splice(0, 0, timeboxToAdd)
-        this.updateTimeboxesInsideCookie();
-
-    },
-    replaceTimebox: async function (timeboxToReplace) {        
-        await wait(3000);
-        this.timeboxes = this.timeboxes.map((value) => value.uid === timeboxToReplace.uid ? timeboxToReplace : value)
-        this.updateTimeboxesInsideCookie();        
-    },
-    removeTimebox: async function (uid) {
-        console.log("Api.removetimebox 1");
-        await wait(1000);
-        this.timeboxes = this.timeboxes.filter((value) => value.uid === uid ? false : true);
-        console.log("Api.removetimebox 2");
-        this.updateTimeboxesInsideCookie();
-        console.log(cookies.get(COOKIE_TIMEBOXES, { path: '/' }));
-    },
-    updateTimeboxesInsideCookie: function () {
-        cookies.set(COOKIE_TIMEBOXES, this.timeboxes, { path: '/' });
-    }
-};
+import { TimeboxFakeAPI as TimeboxAPI } from '../api/TimeboxFakeAPI';
 
 function Pomodoro() {
 
