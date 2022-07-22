c0b78aad4a9ba97c8be0408e73f115e8f723b8c2
mkiszka <github@kiszka.name>
2022-07-22 19:06:33 +0200

    Repaired tests Timebox

--- src/__tests__/components/Timebox.js
+++ src/__tests__/components/Timebox.js
@@ -52,97 +52,101 @@ describe('Timebox', () => {
         let pauseButton = screen.queryByRole('button', { name: /pause/i });
         expect(pauseButton).toBeNull()
 
-        
+
         userEvent.click(playButton);
-        
+
         pauseButton = screen.queryByRole('button', { name: /pause/i });
         expect(pauseButton).toBeNull();
     });
     describe('and its timers', () => {
-            beforeEach(() => {
-                jest.useFakeTimers();
-            });
+        beforeEach(() => {
+            jest.useFakeTimers();
+        })
+        it('should render proper time string',async () => {
+            const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 3 }
 
-            it('should render proper time string', () => {
-                const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 3 }
-
-                render(<Timebox timebox={timebox} isEditable={true} />)
-                const playButton = screen.getByRole('button', { name: /play/i });
-                let clock = screen.queryByText(/00:03:00\.000/i);
-                expect(clock).not.toBeNull();
-
-                userEvent.click(playButton);
-                act(() => {
-                    jest.advanceTimersByTime(60000)
-                })
-
-                clock = screen.queryByText(/00:02:00\.000/i); //Getting it again isn't necessary.
-                expect(clock).not.toBeNull();
-
-                act(() => {
-                    jest.advanceTimersByTime(120000)
-                })
-
-                clock = screen.queryByText(/00:00:00\.000/i); //Getting it again isn't necessary.
-                expect(clock).not.toBeNull();
-
-            });
-            it('should render proper progressbar length', () => {
-                const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 10 }
-
-                render(<Timebox timebox={timebox} isEditable={true} progressBarAriaLabel='Postęp ładowania timeboxów' />)
-                const playButton = screen.getByRole('button', { name: /play/i });
-                let progressbar = screen.getByLabelText('Postęp ładowania timeboxów')
-                let style = window.getComputedStyle(progressbar);
-
-                expect(style._values['--width']).toEqual('100%');
-                userEvent.click(playButton);
-
-                act(() => {
-                    jest.advanceTimersByTime(300000);
-                })
-
-                style = window.getComputedStyle(progressbar);
-                expect(style._values['--width']).toEqual('50%');
-
-                act(() => {
-                    jest.advanceTimersByTime(300000);
-                })
-
-                style = window.getComputedStyle(progressbar);
-                expect(style._values['--width']).toEqual('0%');
-
-
-            });
-            it('should not change progress bar as component is not editable', () => {
-                const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 10 }
-
-                render(<Timebox timebox={timebox} isEditable={false} progressBarAriaLabel='Postęp ładowania timeboxów' />)
-                const playButton = screen.getByRole('button', { name: /play/i });
-                let progressbar = screen.getByLabelText('Postęp ładowania timeboxów')
-                let style = window.getComputedStyle(progressbar);
-
-                expect(style._values['--width']).toEqual('100%');
-                userEvent.click(playButton);
-
-                act(() => {
-                    jest.advanceTimersByTime(300000);
-                })
-
-                style = window.getComputedStyle(progressbar);
-                expect(style._values['--width']).toEqual('100%');
-
-                act(() => {
-                    jest.advanceTimersByTime(300000);
-                })
-
-                style = window.getComputedStyle(progressbar);
-                expect(style._values['--width']).toEqual('100%');
+            render(<Timebox timebox={timebox} isEditable={true} />)
+            const playButton = screen.getByRole('button', { name: /play/i });
+            let clock = screen.queryByText(/00:03:00\.000/i);
+            expect(clock).not.toBeNull();
+            
+            userEvent.click(playButton);
+            //TODO resolve  problem with act warning
+            //https://stackoverflow.com/questions/71681055/react-testing-library-with-userevent-click-wrong-act-warning
+            //https://github.com/testing-library/user-event/issues/255#issuecomment-636412774
+            //niżej testy mają to samo
+        
+            act(() => {
+                jest.advanceTimersByTime(60000)
             })
-            afterEach(() => {
-                jest.runOnlyPendingTimers();
-                jest.useRealTimers();
+
+            clock = screen.queryByText(/00:02:00\.000/i); //Getting it again isn't necessary.
+            expect(clock).not.toBeNull();
+
+            act(() => {
+                jest.advanceTimersByTime(120000)
             })
+
+            clock = screen.queryByText(/00:00:00\.000/i); //Getting it again isn't necessary.
+            expect(clock).not.toBeNull();
+
         });
+        it('should render proper progressbar length', () => {
+            const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 10 }
+
+            render(<Timebox timebox={timebox} isEditable={true} progressBarAriaLabel='Postęp ładowania timeboxów' />)
+            const playButton = screen.getByRole('button', { name: /play/i });
+            let progressbar = screen.getByLabelText('Postęp ładowania timeboxów')
+            let style = window.getComputedStyle(progressbar);
+
+            expect(style._values['--width']).toEqual('100%');
+            userEvent.click(playButton);
+
+            act(() => {
+                jest.advanceTimersByTime(300000);
+            })
+
+            style = window.getComputedStyle(progressbar);
+            expect(style._values['--width']).toEqual('50%');
+
+            act(() => {
+                jest.advanceTimersByTime(300000);
+            })
+
+            style = window.getComputedStyle(progressbar);
+            expect(style._values['--width']).toEqual('0%');
+
+
+        });
+        it('should not change progress bar as component is not editable', () => {
+            const timebox = { uid: 'aaaa-dddd-cccc', title: 'Wywołanie eventów', totalTimeInMinutes: 10 }
+
+            render(<Timebox timebox={timebox} isEditable={false} progressBarAriaLabel='Postęp ładowania timeboxów' />)
+            const playButton = screen.getByRole('button', { name: /play/i });
+            let progressbar = screen.getByLabelText('Postęp ładowania timeboxów')
+            let style = window.getComputedStyle(progressbar);
+
+            expect(style._values['--width']).toEqual('100%');
+            userEvent.click(playButton);
+
+            act(() => {
+                jest.advanceTimersByTime(300000);
+            })
+
+            style = window.getComputedStyle(progressbar);
+            expect(style._values['--width']).toEqual('100%');
+
+            act(() => {
+                jest.advanceTimersByTime(300000);
+            })
+
+            style = window.getComputedStyle(progressbar);
+            expect(style._values['--width']).toEqual('100%');
+        })
+        afterEach(() => {
+            jest.runOnlyPendingTimers();
+            jest.useRealTimers();
+        })
+    });
 
 });
