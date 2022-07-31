3676db5b8420cfb93d07caf274b72302462c0a2f
mkiszka <github@kiszka.name>
2022-07-31 21:30:29 +0200

    fixed adding new timebox

--- src/api/ManagedListAPI.js
+++ src/api/ManagedListAPI.js
@@ -7,5 +7,6 @@ class ManagedListAPI {
     removeElement = async (accessToken, uid) => { this.timeboxesAPI.removeTimebox(accessToken, uid); };
     getAllElements = async (accessToken) => { return this.timeboxesAPI.getAllTimeboxes(accessToken) };
     replaceElement = async (accessToken, timeboxToReplace) => { this.timeboxesAPI.replaceTimebox(accessToken, timeboxToReplace) };
+    addElement = async (accessToken, timeboxToAdd) => { this.timeboxesAPI.addTimebox(accessToken,timeboxToAdd)};
 }
 export default ManagedListAPI;
