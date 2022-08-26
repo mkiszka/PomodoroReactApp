419bc0caf20ffbd126bb8f7eb2c22afd9d36ac31
mkiszka <github@kiszka.name>
2022-08-26 21:00:12 +0200

    add authentication by JWToken

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,42 @@ import LoginForm from './LoginForm';
 import AuthenticationContext from '../contexts/AuthenticationContext';
 import UnauthenticationContext from '../contexts/UnauthenticationContext';
 
+import AuthenticationAPI from '../api/FetchAuthenticationAPI';
+
 const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
 
 //IMPORTANT ! The login functionality is only a simulation.
 function App() {
-    const [isLogged, setIsLogged] = useState(false);
-    const [accessToken/*, setAccessToken*/] = useState('aa-bb-cc');
-    //https://stackoverflow.com/questions/41030361/how-to-update-react-context-from-inside-a-child-component
-    function handleLogout() {        
-        setIsLogged(false);
+        
+    const [accessToken, setAccessToken] = useState(null);
+    const [previousLoginAttemptFailed, setPreviousLoginAttemptFailed ] = useState(false);
+    const isUserLoggedIn = () => { 
+        return !!accessToken;
     }
 
 
-    function handleLogin(data) {        
-        setIsLogged(true);
+    //https://stackoverflow.com/questions/41030361/how-to-update-react-context-from-inside-a-child-component
+    function handleLogout() {        
+        setAccessToken(null);
+        localStorage.removeItem('accessToken');
+    }
+
+
+    function handleLogin(credencials) {     
+        AuthenticationAPI.login(credencials)
+            .then(({accessToken, user})=> { 
+                console.log(accessToken,user); 
+                setAccessToken(accessToken);  
+                localStorage.setItem('accessToken',accessToken);
+            })
+            .catch(()=> { setPreviousLoginAttemptFailed(true)});
+        console.log(credencials);        
     }
 
     return (
         <div id="App" className="App">           
             <ErrorBoundary>
-                {isLogged ?
+                {isUserLoggedIn() ?
                     <AuthenticationContext.Provider value={{ accessToken: accessToken, onLogout: handleLogout }}>
                         <React.Suspense fallback={'Loading ...'}>
                             <AuthenticatedApp />
@@ -33,7 +49,7 @@ function App() {
                     :
                     // ki3 pytanie o w8 l3 i haczyk
                     <UnauthenticationContext.Provider value={{ onLoginAttempt: handleLogin }}>
-                        <LoginForm />
+                        <LoginForm errorMessage={previousLoginAttemptFailed?"Masz jakiś problem !":null} />
                     </UnauthenticationContext.Provider>
                 }
             </ErrorBoundary>
