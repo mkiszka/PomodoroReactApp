c0744a587f387b91d0382fa4e3a73e84dbf4ad91
mkiszka <github@kiszka.name>
2022-08-27 10:32:52 +0200

    clean up

--- src/hooks/useManagedList.js
+++ src/hooks/useManagedList.js
@@ -3,33 +3,33 @@ import { useEffect, useState } from 'react';
 import { getUserId } from "../utilities/accessToken";
 
 
-function useManagedList(elements,setElements,elementAPI) {
+function useManagedList(elements, setElements, elementAPI) {
     console.log('useManagedList')
     const { accessToken: apiAccessToken } = useAuthenticationContext();
     const [isLoading, setIsLoding] = useState(true);
     const [loadingError, setLoadingError] = useState(null);
 
-      
+
     // const TimeboxAPI = useTimeboxAPI();
-   //const [TimeboxAPI] = useTimeboxAPI();
-    
+    //const [TimeboxAPI] = useTimeboxAPI();
+
     useEffect(() => {
         //ki3 - czy tutaj dostęp do Api w zasadzie taki singleton troszkę
         //którego nie da się zamocować przy testach, czy nie powinien być 
         //przekazywany z zewnątrz ?
         elementAPI.getAllElements(apiAccessToken)
-            .then((fetchedElements) => { 
-                setElements(fetchedElements);            
+            .then((fetchedElements) => {
+                setElements(fetchedElements);
             })
             .catch((error) => setLoadingError(error))
             .finally(() => setIsLoding(false));
     }, [apiAccessToken, elementAPI, setElements]); //ki3 po przejściu na hooka wymusza mi tutaj dodanie TimeboxAPI, czy to naprawde musi być?    
 
-    function handleDeleteListElement(deletedElement) {    
-            
+    function handleDeleteListElement(deletedElement) {
+
         elementAPI.removeElement(apiAccessToken, deletedElement).then(() => {
             setElements(
-                (prevTimeboxes) => {                    
+                (prevTimeboxes) => {
                     return prevTimeboxes.filter((value, index) => value.uid === deletedElement.uid ? false : true);;
                 }
             )
@@ -38,14 +38,14 @@ function useManagedList(elements,setElements,elementAPI) {
     }
 
     function handleSaveListElement(editedElement) {
-        editedElement.userId =  getUserId(apiAccessToken);     
-        const promise =  elementAPI.replaceElement(apiAccessToken, { ...editedElement });  
+        editedElement.userId = getUserId(apiAccessToken);
+        const promise = elementAPI.replaceElement(apiAccessToken, { ...editedElement });
         promise.then(
             (replacedElement) => {
                 debugger;
                 setElements(
                     (prevElements) => {
-                        
+
                         return prevElements.map((value) => { //TODOa1 tego mapa spróbować zedytować według uwag z konsultacji
                             return value.uid === editedElement.uid ? { ...editedElement } : value
                         })
@@ -55,9 +55,9 @@ function useManagedList(elements,setElements,elementAPI) {
         )
         return promise;
     }
-    
+
     function handleCreatorAdd(timeboxToAdd) {
-        timeboxToAdd.userId =  getUserId(apiAccessToken);        
+        timeboxToAdd.userId = getUserId(apiAccessToken);
         elementAPI.addElement(apiAccessToken, { ...timeboxToAdd }).then((timeboxAdded) => {
             setElements(
                 (prevTimeboxes) => {
@@ -67,18 +67,18 @@ function useManagedList(elements,setElements,elementAPI) {
         });
     }
 
-    const [currentIndex, setCurrentIndex] = useState(0); 
-    
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     function handleStartListElement(id) {
         setCurrentIndex(id);
         //TODOa1 refactor w/w handlerów z (id) na findElement z hooka useDND ? 
         //findElement do czegoś wspólnego przenieść ?
     }
 
-    return { 
+    return {
         isLoading,
-        loadingError,      
-        elements:  ((elements.length > 0) ? elements[currentIndex] : null),
+        loadingError,
+        elements: ((elements.length > 0) ? elements[currentIndex] : null),
         handleCreatorAdd,
         handleDeleteListElement,
         handleSaveListElement,
