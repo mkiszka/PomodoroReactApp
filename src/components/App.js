91d47e4700dc3daa4cc1190d5432ee9d3447437a
mkiszka <github@kiszka.name>
2022-09-10 20:52:30 +0200

    refactor UserGreeting to use redux state

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,8 @@ import LoginForm from './LoginForm';
 import AuthenticationContext from '../contexts/AuthenticationContext';
 import UnauthenticationContext from '../contexts/UnauthenticationContext';
 
-
-import ManagedListAPI from "../api/ManagedListAPI";
-import { AxiosTimeboxAPI } from "../api/AxiosTimeboxAPI";
-
 import { useDispatch, useSelector } from 'react-redux';
-import { getAccessToken, getPreviousLoginAttemptFailed, isAccessTokenExpired } from '../redux/authentificationActions';
+import { getPreviousLoginAttemptFailed, isAccessTokenExpired } from '../redux/authentificationActions';
 import { deleteAccessToken, loginToApi } from '../redux/authentificationReducer';
 
 const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
@@ -18,8 +14,7 @@ const AuthenticatedApp = React.lazy(() => import('./AuthenticatedApp'));
 //function stateReducer = (state,action) => newState;
 
 function App() {
-    const dispatch = useDispatch();
-    const accessToken = useSelector(getAccessToken);    
+    const dispatch = useDispatch();    
     const previousLoginAttemptFailed = useSelector(getPreviousLoginAttemptFailed);    
     const isUserLoggedIn = useSelector(isAccessTokenExpired);
                             
@@ -37,7 +32,7 @@ function App() {
                 {isUserLoggedIn ?
                     //ki4 - czy trzymanie API w contexcie to dobry pomysł? Np gdy używawm mangedListApi w różnych miejscach to nie muszę apamiętać co za każdym razem do konstruktora przekazywać
                     //gdyby to była większa konfiguracja to by trzeba było za każdym razem tworzyć od nowa.
-                    <AuthenticationContext.Provider value={{ accessToken: accessToken, onLogout: handleLogout, managedListAPI: new ManagedListAPI(AxiosTimeboxAPI) }}>
+                    <AuthenticationContext.Provider value={{ onLogout: handleLogout }}>
                         <React.Suspense fallback={'Loading ...'}>
                             <AuthenticatedApp />
                         </React.Suspense>
--- src/utilities/accessToken.js
+++ src/utilities/accessToken.js
@@ -1,16 +1,28 @@
 import jwt_decode from "jwt-decode";
 
 function getUserEmail(accesToken) {    
+    if(accesToken === null) {
+        return '';
+    }
+    
     const { email } = jwt_decode(accesToken);
     return email;
 }
 
 function getUserId(accesToken) {
+    if(accesToken === null) {
+        return null;
+    }
+    
     const { sub } = jwt_decode(accesToken);
     return sub;
 }
 
 function isExpired(accesToken) {        
+    if(accesToken === null) {
+        return true;
+    }
+    
     const { exp } = jwt_decode(accesToken);
     const now = new Date().getTime();
 
