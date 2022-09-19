1b1bc42e6b944cee3b875ada9408ab1d5940d724
mkiszka <github@kiszka.name>
2022-09-19 17:19:37 +0200

    add login view

--- src/index.js
+++ src/index.js
@@ -2,19 +2,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./styles/main.scss"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
 
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 import { CookiesProvider } from 'react-cookie'
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './redux';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+import Auth from "layouts/Auth.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ReduxProvider store={store}>
             <CookiesProvider>
-                <App />
+            <BrowserRouter>
+                <Routes>
+                {/* add routes with layouts */}
+                {/* <Route path="/admin" component={Admin} /> */}
+                <Route path="/auth/*" element={<Auth/>} />
+                {/* add routes without layouts */}
+                {/* <Route path="/landing" exact component={Landing} /> */}
+                {/* <Route path="/profile" exact component={Profile} /> */}
+                {/* <Route path="/" exact component={Index} /> */}
+                {/* add redirect for first page */}
+                <Route 
+                    path="*"
+                    element={<Navigate to="/auth" replace/>}
+                />
+                </Routes>
+            </BrowserRouter>,
+               
             </CookiesProvider>
         </ReduxProvider>
     </React.StrictMode>
