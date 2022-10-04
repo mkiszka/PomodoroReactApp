14793cc4661bb710c4234c081e91788e2d82ba7c
mkiszka <github@kiszka.name>
2022-10-04 11:40:30 +0200

    add unique key into InspirationQuoteContainer

--- src/layouts/InspirationQuoteContainer.js
+++ src/layouts/InspirationQuoteContainer.js
@@ -1,25 +1,25 @@
 import React from "react";
-
+import { v4 as uuidv4 } from "uuid"
 
 export default function InspirationQuoteContainer({
   children
 }) {
 
   const childrenArray = React.Children.toArray(children);
-  
+
   return (
     <>
       <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12">
-        <div className="px-4 md:px-10 mx-auto w-full">        
-          <div className="flex flex-wrap justify-center">  
+        <div className="px-4 md:px-10 mx-auto w-full">
+          <div className="flex flex-wrap justify-center">
             {
               childrenArray.map(elem => {
-                return <div className="w-full lg:w-4/12 xl:w-3/12 px-4">
-                    <elem.type />
-                  </div>                
+                return <div key={uuidv4()} className="w-full lg:w-4/12 xl:w-3/12 px-4">
+                  <elem.type />
+                </div>
               })
-            }   
-            </div>;       
+            }
+          </div>;
         </div>
       </div>
     </>
